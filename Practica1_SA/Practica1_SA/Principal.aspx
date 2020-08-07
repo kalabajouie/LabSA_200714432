62a19cd713136bc088554464107c685883f2b5af
kalabajouie <200714432lh@gmail.com>
2020-08-07 16:17:04 -0600

    Carga inicial
    Carga del proyecto de .net de la practica 1 de SA

---
+++ Practica1_SA/Practica1_SA/Principal.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="Principal.aspx.vb" Inherits="Practica1_SA.Principal" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+
+    <link href="css/datatables.css" rel="stylesheet" type="text/css" />
+    
+    <script src="js/jquery-3.3.1.js" type="text/javascript"></script>
+    <script src="js/datatables.js" type="text/javascript"></script>
+    <script src="js/dataTables.responsive.js" type="text/javascript"></script>
+    
+    <script type="text/javascript">
+        $(function () {
+            $("[id*=GridView1]").prepend($("<thead></thead>").append($('#GridView1').find("tr:first"))).DataTable();
+
+        });
+</script>
+
+    <title></title>
+</head>
+<body style="background-color:Gray;">
+    <form id="form1" runat="server">
+    <div>
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Practica1_SA/Practica1_SA/Principal.aspx.vb
@@ -0,0 +1,44 @@
+﻿Imports Practica1_SA.api_sa_admin_contactos
+
+Public Class Principal
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Try
+            Dim api As New administratorcontact100Client
+
+            'Se crean 10 contactos
+            'For i As Integer = 0 To 10 - 1
+            '    api.create("200714432 - Contacto " & i, 0, "", 1)
+            'Next
+
+
+            Dim filtro As New readList_filters
+            filtro.published = 1
+
+            Dim obj = api.readList(0, 10, "200714432", filtro, "", "", "")
+
+            Dim dt As New DataTable
+            dt.Columns.Add("id")
+            dt.Columns.Add("access")
+            dt.Columns.Add("featured")
+            dt.Columns.Add("language")
+            dt.Columns.Add("name")
+            dt.Columns.Add("ordering")
+
+
+            For i As Integer = 0 To obj.Count - 1
+                dt.Rows.Add(obj(i).id, obj(i).access, obj(i).featured, obj(i).language, obj(i).name, obj(i).ordering)
+            Next
+
+            GridView1.DataSource = dt
+            GridView1.DataBind()
+
+        Catch ex As Exception
+
+        End Try
+        
+
+    End Sub
+
+End Class
